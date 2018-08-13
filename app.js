e0972993c61aaa00226df5d1212c3789cb1ef28b
JT Parker <jparkerdev@gmail.com>
2018-08-13 13:55:53 -0600

    objects built working on click functionality

--- app.js
+++ app.js
@@ -7,25 +7,56 @@ let Cat = function(name, src) {
   this.catImageElem = document.getElementById('cat-pic');
 };
 
-cat.prototype.selectCat = function() {
+Cat.prototype.selectCat = function() {
   this.clickCounterElem.textContent = this.clicks;
   this.catNameElem.textContent = this.name;
   this.catImageElem.src = this.image;
 };
 
-cat.prototype.clickCat = function() {
+Cat.prototype.clickCat = function() {
   this.clicks++;
   this.clickCounterElem.textContent = this.clicks;
-}
+};
 
 let cat1 = new Cat('Fluffy', 'images/cat.jpg');
 let cat2 = new Cat('Furry', 'images/cat2.jpg');
-let cat3 = new Cat('Hairy', 'images/cat3.jpg');
-let cat4 = new Cat('Baldy', 'images/cat4.jpg');
+let cat3 = new Cat('Hairy', 'images/cat3.jpeg');
+let cat4 = new Cat('Baldy', 'images/cat4.jpeg');
 let cat5 = new Cat('Mittens', 'images/cat5.jpg');
 
-let init = cat1.click();
-
-window.onload = function() {
+document.onload = function() {
   init();
-}
+};
+
+let init = cat1.selectCat();
+
+
+document.addEventListener('click', function(e) {
+  let target = e.target;
+
+  if (target.matches('.one')) {
+    cat1.selectCat();
+  }
+
+  if (target.matches('.two')) {
+    cat2.selectCat();
+  }
+
+  if (target.matches('.three')) {
+    cat3.selectCat();
+  }
+
+  if (target.matches('.four')) {
+    cat4.selectCat();
+  }
+
+  if (target.matches('.five')) {
+    cat5.selectCat();
+  }
+
+  if (target.matches('#cat-pic')) {
+    cat.clickCat();
+  }
+
+
+},false);
